051afa292642385ea4d6eee5527fea1a14981d24
William1099 <williamliam1099@gmail.com>
2020-09-16 00:44:55 +0700

    revert changes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
 
 $(document).ready(function() {
     
-    $(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-    });
     
     // header
 
